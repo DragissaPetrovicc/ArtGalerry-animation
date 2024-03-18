30fa48e0b86ff62ba5bd538fa8f02212d0230cdf
Dragiša Petrović <dragissapetrovicc@gmail.com>
2024-03-18 17:43:48 +0100

    Add files via upload

---
+++ ArtGalerry.js
@@ -0,0 +1,6 @@
+const instagram_button = document.getElementById("instagram");
+
+instagram_button.addEventListener("click" , function(){
+
+    const instagram = window.open("https://www.instagram.com/");
+});
